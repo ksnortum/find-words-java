f00cff88ef5c51bf70d3655b7ea62ebd8d450ec0
Knute Snortum <knute@snortum.net>
2015-08-20 08:58:58 -0700

    Use Map instead of List for speed

--- src/main/java/net/snortum/scrabble_words/model/Dictionary.java
+++ src/main/java/net/snortum/scrabble_words/model/Dictionary.java
@@ -5,9 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
